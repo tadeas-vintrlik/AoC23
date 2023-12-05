dd73fc78318ad1283467aa1f936a5ad92163b4c8
tadeas-vintrlik <tadeas.vintrlik@posteo.net>
2023-12-05 18:05:29 +0100

    day5 part1
    Signed-off-by: tadeas-vintrlik <tadeas.vintrlik@posteo.net>

--- day5/day5.py
+++ day5/day5.py
@@ -4,26 +4,38 @@ import re
 import itertools
 
 
-def range_almanac_map(nums):
-    nums = list(map(int, re.findall(r'\d+', nums)))
-    return (
-        range(nums[0], nums[0] + nums[2]),
-        range(nums[1], nums[1] + nums[2]),
-    )
+def num_in_range(num, rg):
+    dst, src, l = rg
+    return num in range(src, src + l)
+
+
+def translate_numbers(nums, map_part):
+    for i, num in enumerate(nums):
+        for rg in map_part:
+            if num_in_range(num, rg):
+                dst, src, l = rg
+                nums[i] = dst + (nums[i] - src)
+
+    return nums
 
 
 def part1(seeds, maps):
-    pass
+    next = seeds
+    for mp in maps:
+        next = translate_numbers(next, mp)
+
+    return(min(next))
 
 
 def main():
-    with open('test.input', encoding='utf-8') as f:
+    with open('main.input', encoding='utf-8') as f:
         lines = [line.strip() for line in f.readlines()]
-    seeds = {int(seed) for seed in re.findall(r'\d+', lines[0])}
+    seeds = [int(seed) for seed in re.findall(r'\d+', lines[0])]
 
     maps = [ list(group)[1:] for k, group in itertools.groupby(lines[2:], lambda x: x == '') if not k]
-    maps = [ [ range_almanac_map(line) for line in mp ] for mp in maps]
-    print(maps)
+    maps = [ [ tuple(map(int, line.split())) for line in mp ] for mp in maps ]
+
+    print(part1(seeds, maps))
 
 
 if __name__ == "__main__":
